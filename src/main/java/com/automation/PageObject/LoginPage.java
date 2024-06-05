8fd4eb3713fe9ad0473c2bee4a55f97f802e9c8c
Mahmoud EL AALLOULI <elaallouli.mahmoud@gmail.com>
2024-06-05 19:47:37 +0200

    First commit git ignore

---
+++ src/main/java/com/automation/PageObject/LoginPage.java
@@ -0,0 +1,21 @@
+package com.automation.PageObject;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.annotations.findby.FindBy;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage extends PageObject {
+
+    // ============================ OBJECTS ============================
+
+    @FindBy(xpath = "//input[@data-qa='login-email']")
+    public WebElement loginInput;
+
+    @FindBy(xpath = "//input[@data-qa='login-password']")
+    public WebElement passwordInput;
+
+    @FindBy(xpath = "//button[@data-qa='login-button']")
+    public WebElement LoginBtn;
+
+
+}
